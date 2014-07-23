2a8d709bde6655ea39d25ef5a170e094d258c7f0
SterlingVix <amelocik@gmail.com>
2014-07-23 12:15:14 -0700

    Primary changes to functional-shared queue and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,43 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  //someInstance.size = 0;
+  var size = 0;
+  //var lastIn = 0; // if I want to track "size" and keys separately...
+  var location = 1; // to track location of growing stack
+  /* var resize() = function() {
+    resize stack after certain point...
+  } */
+  var queueThreshold = 256; // double this every time it's met if resizing...
+  
   // Implement the methods below
-
   someInstance.enqueue = function(value){
+    if (typeof value !== 'string') {
+      console.log("Error! var value of " + value + " is not a string.");
+      return false;
+    }
+    // IF RESIZING: if (size === queueThreshold) ...
+    
+    size++; // increment size tracker
+    storage[size] = value;
+    return true; // push is successful
   };
 
   someInstance.dequeue = function(){
+    if (location > size) {
+      console.log("Error! Attempting to dequeue from empty queue.");
+      return false;
+    }
+    var result = storage[location];
+    storage[location] = null;
+    location++; // increase location
+    return result;
   };
 
   someInstance.size = function(){
+    return size - location;
   };
 
+debugger;
   return someInstance;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,36 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  //someInstance.size = 0;
+  var size = 0;
+  
+  
+  
 
   // Implement the methods below
   someInstance.push = function(value){
+    if (typeof value !== 'string') {
+      console.log("Error! var value of " + value + " is not a string.");
+      return false;
+    }
+    size++; // increment size tracker
+    storage[size] = value;
+    return true; // push is successful
   };
 
   someInstance.pop = function(){
+    if (size === 0) {
+      console.log("Error! Attempting to pop from empty stack.");
+      return false;
+    }
+    var result = storage[size];
+    storage[size] = null;
+    size--; // decrement size
+    return result;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
